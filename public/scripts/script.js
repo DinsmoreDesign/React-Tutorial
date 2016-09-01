f9433eb5a55650e6c393c1ca9aa899cd153d59c0
Derek <derek@dinsmore-design.com>
2016-09-01 16:10:44 -0700

    Adding some styling, so it looks better.

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -114,9 +114,9 @@
       return (
 
         <form className="commentForm" onSubmit={this.handleSubmit}>
-          <input type="text" placeholder="Your name" value={this.state.author} onChange={this.handleAuthorChange} />
-          <input type="text" placeholder="Say something..." value={this.state.text} onChange={this.handleTextChange} />
-          <input type="submit" value="Post" />
+          <input type="text" className="postName" placeholder="Your name" value={this.state.author} onChange={this.handleAuthorChange} />
+          <input type="text" className="postText" placeholder="Say something..." value={this.state.text} onChange={this.handleTextChange} />
+          <input type="submit" className="postSubmit" value="Post" />
         </form>
 
       );
@@ -141,10 +141,14 @@
       return (
 
         <div className="comment">
-          <h2 className="commentAuthor">
-            {this.props.author}
-          </h2>
-          <span dangerouslySetInnerHTML={this.rawMarkup()} />
+          <div className="name">
+            <h2 className="commentAuthor">
+              {this.props.author}:
+            </h2>
+          </div>
+          <div className="text">
+            <span dangerouslySetInnerHTML={this.rawMarkup()} />
+          </div>
         </div>
 
       );
